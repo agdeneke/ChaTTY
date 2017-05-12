c3c5f8c367608fe8dd7fd47903f0ce92df3a9cfc
Abiel Deneke <abiel.deneke@gmail.com>
2017-05-12 17:04:42 -0500

    Start making ncurses interface.

--- client.c
+++ client.c
@@ -17,7 +17,7 @@ int sock;
 
 void exit_client()
 {
-	printf("Exiting ChaTTY...\n");
+	endwin();
 	close(sock);
 	exit(0);
 }
@@ -53,10 +53,19 @@ int main (int argc, char *argv[])
 	send(sock, username, sizeof(username), 0);
 
 	initscr();
+	WINDOW *input_window = newwin(0, 0, 0, 0);
 
 	while (1) {
-		
+		char msg[MAXBUFSIZE];
+		if (recv(sock, &msg, sizeof(msg), 0) <= 0)
+			exit_client();
+		if (!strncmp(msg, "S|", 2)) {
+			wprintw(input_window, "%s", msg+2);
+			wrefresh(input_window);
+		}
 	}
+
+	endwin();
 
 	return 0;
 }
